ed36db7114102b183c9a985c89dcba0fc58e338d
VilkovSergey <vilkov75@yandex.ru>
2023-05-16 16:19:59 +0300

    Создание сайта по макету Figma

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var sidenav = document.querySelectorAll('.sidenav');
+  var instances_sidenav = M.Sidenav.init(sidenav);
+  var modal = document.querySelectorAll('.modal');
+  var instances_modal = M.Modal.init(modal);
+  var gallery = document.querySelectorAll('.materialboxed');
+  var instances_gallery = M.Materialbox.init(gallery);
+});
+
